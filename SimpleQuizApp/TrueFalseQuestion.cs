c5b1016beb79baf557754779c0d452573cf87b42
jseibert <jseibert@css.edu>
2018-09-18 10:36:19 -0500

    Added true false question class

---
+++ SimpleQuizApp/TrueFalseQuestion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuizApplication
+{
+    /**
+     *
+     * @author tgibbons
+     */
+    class TrueFalseQuestion : Question
+    {
+        private Boolean answer;
+
+        /**
+         * Constructor
+         * @param questionText - The text for the True/False answer question
+         * @param answerText - The correct answer for the short answer question, either "True" or "False", case is irrelevant
+         */
+        public TrueFalseQuestion(String questionText, String answerText) : base(questionText)
+        {
+            //super(questionText);
+            answer = answerText.Equals("True");
+        }
+        /**
+         * getAnswer()
+         * @return The correct answer to this question as a single string, either "True" or "False", case is irrelevant
+         */
+        public override string getAnswer()
+        {
+            if (answer)
+            {
+                return ("True");
+            }
+            else
+            {
+                return ("False");
+            }
+        }
+        /**
+         * getQuestion()
+         * @return Adds "True/False" to the question text
+         */
+        public override string getQuestion()
+        {
+            return this.questionText + "(True/False)";
+        }
+        /**
+         * checkAnswer(String givenAnswer)
+         * @param givenAnswer - The answer the user entered to be compared with the correct answer, either "True" or "False"
+         * @return True is givenAnswer matches the correct answer, this is not case-sensitive
+         */
+        public override bool checkAnswer(String givenAnswer)
+        {
+            return (answer == givenAnswer.Equals("True"));
+        }
+
+    }
+}
